c5a042cec90c195f311526a4d3ae9bfb712243e2
GoodBye-G <toyanovi12397@gmail.com>
2026-04-03 12:03:18 +0300

    Fixes and optimize

--- second-homework.py
+++ second-homework.py
@@ -1,106 +1,80 @@
 # #Завдання 1 -Iнiцiали
 
-# name=str(input("Введiть своє iм`я ты прiзвище(Name Surname)"))
-# initials=[]
+# full_name = input("Введiть своє iм`я ты прiзвище(Name Surname)")
 
-# for word in name.split():
-#     initials.append(word[0]) #додаємо у пустий список першу букву з кожного члена списку
-
-# if not name:
-#     print("Перевiрте написане iм`я")
-# else:
-#     #метод .join щоб зробити з списку назад рядок (str)
-#     print(f"Все добре вот ваши iнiцiали: {" ".join(initials)}")
-
-
-
+# name , surname = full_name.split() 
+# print (f"Iнiцiали: {name[0]}, {surname[0]}")
 
 
 
 # #Завдання 2 -Маскування email
-# mail=input("Введiть свою електрону пошту: ")
+# mail = input("Введiть свою електрону пошту: ")
 
-# if mail[-4:] != ".com" and mail[-4:] != ".org":
-#     print("Пошта не закiнчується на .org та .com")
-# elif not mail:
-#     print("Напешiть будь ласка пошту")
+# if not mail or not mail.endswith((".com" , ".org")):
+#     print ("Неправильно написана пошта")
 # else:
 #     pass
 
-# com=0
-# for letters in mail:
-#     if letters != "@":
-#         com=com+1
-#     else:
-#         break
 
-# secret_mail=mail[1:com-1]
-# secret_mail=mail.replace(secret_mail, len(secret_mail) * '*')
+# mail_name , domain = mail.split("@")
+
+# secret_mail = mail_name[1:-1]
+# secret_mail = mail.replace(secret_mail, len(secret_mail) * '*')
 # print(secret_mail)
 
 
 
     
-# #Завдання 3-Додавання унiкального значення
-# numbers={1,2,3,4,5}
+# #Завдання 3-Додавання унiкального значення з використанням list для створення свого set()
 
-# new_num=int(input("Введiть будь яку цифру: "))
+# numbers = [1,2,3,4,5]
 
-# for num in numbers:
-#     if num==new_num:
-#          print(f"Таке значення вже є, ось як виглядає список: {numbers}")       
-#          break    
-        
+# user_num = int(input("Введiть будь яку цифру: "))
+
+
+# if user_num in numbers:
+#     print ("Таке число вже э")
 # else:
-#         numbers.add(new_num)
-#         print(f"Ось так виглядає новик список з вашою допомогою: {numbers}")
+#     numbers.append(user_num)
+#     print(f"Ось новий список з вашою допомогою {numbers} ")
         
 
 
 
+# #Завдання 4-Аналiз тегiв i персональних iнтересiв кожного юзера
 
+# first_user_interests = input(f"Введiть будь ласка у виглядi тегiв через кому,\n Наприклад: ai, python, music, war\n")
+# second_user_interests = input(f"\nВведiть будь ласка у виглядi тегiв через кому,\n Наприклад: ai, python, music, war\n")
 
+# first_user_tags = set()
+# second_user_tags = set()
 
-# #Завдання 4-Аналiз тегiв
-# user_1=input(f"Введiть будь ласка у виглядi тегiв через кому,\n Наприклад: ai, python, music, war\n")
-# user_2=input(f"\nВведiть будь ласка у виглядi тегiв через кому,\n Наприклад: ai, python, music, war\n")
+# first_user_tags.update((first_user_interests.strip()).split(","))
+# second_user_tags.update((second_user_interests.strip()).split(","))
 
-# tags_1=set()
-# tags_2=set()
+# print(f"Теги першого юзера: {first_user_tags}")
+# print(f"Теги другого юзера: {second_user_tags}")
 
-# tags_1.update((user_1.strip()).split(","))
-# tags_2.update((user_2.strip()).split(","))
+# print(f"Спiльнi iнтереси: {first_user_tags.intersection(second_user_tags)}")
 
-# print(f"Теги першого юзера: {tags_1}")
-# print(f"Теги другого юзера: {tags_2}")
-
-# print(f"Спiльнi iнтереси: {tags_1.intersection(tags_2)}")
-
-# print(f"Унiкальнi iнтереси для першого юзера: {" ,".join(tags_1.difference(tags_2))}\n" )
-# print(f"Унiкальнi iнтереси для другого юзера: {" ,".join(tags_2.difference(tags_1))}\n")
+# print(f"Унiкальнi iнтереси для першого юзера: {" ,".join(first_user_tags.difference(second_user_tags))}\n" )
+# print(f"Унiкальнi iнтереси для другого юзера: {" ,".join(second_user_tags.difference(first_user_tags))}\n")
 
 
 
 
 
 # #Завдання 5-Обробка рядка з числами
-# numbers=input("Введiть значення для суми всiх вказених чисел(вводити через пробiл): ")
-# numbers_list=numbers.split()
+user_nums_for_sum = input("Введiть значення для суми всiх вказених чисел(вводити через пробiл): ")
+numbers_list = user_nums_for_sum.split()
 
-
-
-# element=0; sum=0; count=0
-# for num in range(len(numbers_list)):   
-#     element=numbers_list[num]
-#     if element.isdigit():               
-#         sum+=int(element)               
-#         count+=1                        
-#         if count==3:                    
-#             break        
-#     else:                               
-#         print(f"{element}- НЕ є числом")
-
-# print(f"Сума перших трьох членiв{sum}")
-
+numbers_sum = 0
+for number in numbers_list:
+    if number.isdigit():
+        numbers_sum += int(number)
+    else:
+        print(f"{number}- не є числом")
+        
+print(f"Сумою усiх ваших чисел = {numbers_sum}")
 
 
